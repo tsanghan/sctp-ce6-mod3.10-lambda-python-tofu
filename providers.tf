a50dfd1c608deda437c00b6778896fb7181de739
Wong Tsang Han <tsanghan@gmail.com>
2024-09-17 12:27:07 +0800

    added hashicorp/archive provider

--- providers.tf
+++ providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~>5.66.0"
     }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.6.0"
+    }
   }
   required_version = ">= 1.0"
 }
