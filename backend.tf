91d9a7f4b28ba70e7cc78cd17472901ae903f8f4
Wong Tsang Han <tsanghan@gmail.com>
2024-09-19 12:09:13 +0800

    change backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce6-tfstate"
-    key    = "tsanghan-ce6-mod3_10-Serverless-Python-Tofu.tfstate"
+    key    = "tsanghan-ce6-mod3_10-Lambda-Python-Tofu.tfstate"
     region = "ap-southeast-1"
   }
 }
